9bcf59f95392f09c242ba5003da1c08de4cddd68
Glen Martin <glen18martin@gmail.com>
2017-06-09 11:46:41 +0530

    updated gui and fixed link to website

--- www/js/feed.js
+++ www/js/feed.js
@@ -1,34 +1,34 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
-<!--Device Ready Function-->
+//Device Ready Function
 function onDeviceReady() {
 
-    <!--Opening the link in the inappbrowser-->
+    //Opening the link in the inappbrowser
     $(document).on('click', '#link', function() {
-        var website = $("#link").attr("data-link");
+        var website = $(this).attr('data-link');
         window.open = cordova.InAppBrowser.open;
         var ref = window.open(website, '_blank', 'location=yes');
 
     });
 }
 
-<!--Loading Javascript API-->
+//Loading Javascript API
 google.load("feeds", "1");
 
-<!--Defining initialize method fetching feeds and displaying them-->
+//Defining initialize method fetching feeds and displaying them
 function initialize(url) {
 
-    <!--Calling google's feed method-->
+    //Calling google's feed method
     var feed = new google.feeds.Feed(url);
 
-    <!--Setting the result format = Json Format-->
+    //Setting the result format = Json Format
     feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
 
-    <!--Setting the number of results = 10 -->
+    //Setting the number of results = 10 
     feed.setNumEntries(10);
 
 
-    <!--Loading the feeds-->
+    //Loading the feeds
     feed.load(function(result) {
         var div1 = "";
         var pubdate;
@@ -36,7 +36,7 @@ function initialize(url) {
             for (var i = 0; i < result.feed.entries.length; i++) {
                 var entry = result.feed.entries[i];
 
-                <!--Grabbing Blog URL, Blog title, Blog Category and Publish Date and displaying them-->
+                //Grabbing Blog URL, Blog title, Blog Category and Publish Date and displaying them
                 div1 += "<li><a class='ui-btn ui-btn-icon-right ui-icon-carat-r' id=\"link\" data-link=\"" + entry.link + "\">" + entry.title + "<p id=\"catp\"><strong id=\"cat\">" + entry.categories + "</strong></p><p>" + entry.publishedDate + "</p>" + "</a></li>";
                 document.getElementById("myTable").innerHTML = div1;
             }
@@ -44,7 +44,7 @@ function initialize(url) {
     });
 }
 
-<!--Grabbing Feed URL entered by the user-->
+//Grabbing Feed URL entered by the user
 $(document).ready(function() {
     $("#getfeed").click(function() {
         var url = $("#feedurl").val();
